aa79e008048aa7b4b22d0982437217466f9db913
john-emerson14 <john.carv.sousa@gmail.com>
2020-03-08 13:44:47 -0300

    chore: start javascript code

---
+++ script.js
@@ -0,0 +1,24 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+
+// Adiciona zero inicial aos números <= 9 (apenas para estética):
+
+
+// Executa um timer padrão de minuto / segundo / centésimos:
+
+
+// Verifica texto digitado com o fornecido na página:
+
+
+// Inicia o cronômetro:
+
+
+// Função de recomeçar:
+
+
+// Listeners de eventos para entrada de teclado e o botão de recomeçar:
+
